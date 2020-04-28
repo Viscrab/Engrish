02b1508f35e23be3d8a1bb22a83a9b15ba015e42
Jeffrey <Jeffrey@192.168.0.17>
2020-04-28 11:49:08 -0400

    gvgv

---
+++ adjhasdhnkasf/src/adjhasdhnkasf/Adjhasdhnkasf.java
@@ -0,0 +1,12 @@
+package adjhasdhnkasf;
+public class Adjhasdhnkasf {
+    public static void main(String[] args) {
+        Double wa = Math.random();
+        if (wa > 0.5) {
+            System.out.println("Juefeigay");
+        } else {
+            System.out.println("Juefeibad");
+        }
+    }
+    
+}
